311229f77afb00d861a991aec5c425ab308afd19
Inseok Lee <inska@lisky.net>
2014-12-01 10:08:30 +0900

    rebranding for wincpp

--- CSharp/TP2_WINCPP_TEST/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// TP2_WINCPP_TEST.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
