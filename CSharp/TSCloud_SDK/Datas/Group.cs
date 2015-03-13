b28362cf889d71de222fd585b2afc5ad6fc9195d
Inseok Lee <inska@lisky.net>
2015-03-13 16:48:15 -0400

    Group

---
+++ CSharp/TSCloud_SDK/Datas/Group.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using TDSPRINT.Cloud.SDK.Types;
+using Newtonsoft.Json;
+
+namespace TDSPRINT.Cloud.SDK.Datas
+{
+    public class Groups : CommonList
+    {
+        private List<Group> m_contents;
+        [JsonProperty("contents")]
+        public List<Group> Contents
+        {
+            get
+            {
+                return m_contents;
+            }
+            set
+            {
+                m_contents = value;
+            }
+        }
+    }
+
+    public class Group : CommonItem
+    {
+        #region constructor
+        public Group(HttpStatusCode status_code, string strMessage = null)
+            : this()
+        {
+            Message = strMessage;
+            StatusCode = status_code;
+        }
+        public Group(string strMessage)
+            : this()
+        {
+            Message = strMessage;
+        }
+        public Group()
+        {
+        }
+        #endregion
+
+        #region properties
+        [JsonProperty("users")]
+        public List<User> Users { get; set; }
+        
+        [JsonProperty("description")]
+        public String Description { get; set; }
+
+        [JsonProperty("team")]
+        public Team Team { get; set; }
+        #endregion
+
+        #region method
+        public String GetUserIds()
+        {
+            var user_ids = Users.Select( user => user.Id ).ToArray();
+            return String.Join(",", user_ids.Select( x => x.ToString()).ToArray());
+        }
+        #endregion
+    }
+}
---
+++ CSharp/TSCloud_SDK/GroupClient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+
+using TDSPRINT.Cloud.SDK.Datas;
+using TDSPRINT.Cloud.SDK.Types;
+
+namespace TDSPRINT.Cloud.SDK
+{
+    public class GroupClient : TSCloud
+    {
+        #region member variables
+        private Hash Configuration;
+        #endregion
+
+        #region constructor
+        public GroupClient()
+        {
+            this.Configuration = null;
+        }
+        public GroupClient(TSCloud TSCloud) : this()
+        {
+            Hostname = TSCloud.Hostname;
+            RestClient = new RestClient(Hostname);
+            ApiToken = TSCloud.ApiToken;
+            CurrentUser = TSCloud.CurrentUser;
+
+            // Default Configuration
+            Configuration = new Hash();
+            Configuration["PerPage"] = 30;
+        }
+        public GroupClient(TSCloud TSCloud, Hash Configuration)
+            : this(TSCloud)
+        {
+            this.Configuration = Configuration;
+        }
+        #endregion
+
+        #region
+        public Group GetGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Create(Group group)
+        {
+            RestRequest request = new RestRequest(String.Format("{0}/groups", ApiPath), Method.POST);
+            request.AddParameter("api_token", ApiToken);
+            request.AddParameter("name", group.Name);
+            request.AddParameter("description", group.Description);
+            request.AddParameter("acl", group.Acl.Stringify());
+            request.AddParameter("user_ids", group.GetUserIds());
+
+            Group created_group = new Group();
+
+            try
+            {
+                IRestResponse httpResponse = RestClient.Execute(request);
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    created_group = JsonConvert.DeserializeObject<Group>(httpResponse.Content, TSCloud.serializer_settings());
+                    created_group.StatusCode = httpResponse.StatusCode;
+
+                    return created_group;
+                }
+                else
+                {
+                    created_group.StatusCode = httpResponse.StatusCode;
+                    created_group.Message = httpResponse.ErrorMessage;
+
+                    return created_group;
+                }
+            }
+            catch (Exception ee)
+            {
+                return new Group(Convert.ToString(ee));
+            }
+        }
+
+        public bool Delete(int GroupID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Update(Group group)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Get(int GroupID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Copy(Group group)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Move(Group group)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
