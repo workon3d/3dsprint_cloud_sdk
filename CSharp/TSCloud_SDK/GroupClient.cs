1696f411b85c5b516ca26c42c8f4e839891f18cf
Inseok Lee <inska@lisky.net>
2015-03-13 17:03:43 -0400

    GroupClient basic method

--- CSharp/TSCloud_SDK/GroupClient.cs
+++ CSharp/TSCloud_SDK/GroupClient.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Text;
 using RestSharp;
 using Newtonsoft.Json;
-
 using TDSPRINT.Cloud.SDK.Datas;
 using TDSPRINT.Cloud.SDK.Types;
 
@@ -42,9 +41,37 @@ namespace TDSPRINT.Cloud.SDK
         #endregion
 
         #region
-        public Group GetGroups()
+        public Groups GetGroups(int Page)
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest(String.Format("{0}/groups", ApiPath), Method.GET);
+            request.AddParameter("api_token", ApiToken);
+            if (Page != 0)
+                request.AddParameter("page", Page);
+
+            Groups groups = new Groups();
+
+            try
+            {
+                IRestResponse httpResponse = RestClient.Execute(request);
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    groups = JsonConvert.DeserializeObject<Groups>(httpResponse.Content, TSCloud.serializer_settings());
+                    groups.StatusCode = httpResponse.StatusCode;
+
+                    return groups;
+                }
+                else
+                {
+                    groups.StatusCode = httpResponse.StatusCode;
+                    groups.Message = httpResponse.ErrorMessage;
+
+                    return groups;
+                }
+            }
+            catch (Exception ee)
+            {
+                throw ee;
+            }
         }
 
         public Group Create(Group group)
@@ -82,19 +109,90 @@ namespace TDSPRINT.Cloud.SDK
             }
         }
 
-        public bool Delete(int GroupID)
+        public HttpStatusCode Delete(int GroupID)
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest(String.Format("{0}/groups/{1}", ApiPath, Convert.ToString(GroupID)), Method.DELETE);
+            request.AddParameter("api_token", ApiToken);
+
+            try
+            {
+                IRestResponse httpResponse = RestClient.Execute(request);
+                return httpResponse.StatusCode;
+            }
+            catch (Exception ee)
+            {
+                throw ee;
+            }
         }
 
         public Group Update(Group group)
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest(String.Format("{0}/groups/{1}", ApiPath, Convert.ToString(group.Id)), Method.PUT);
+            request.AddParameter("api_token", ApiToken);
+
+            if (!String.IsNullOrEmpty(group.Name))
+                request.AddParameter("name", group.Name);
+            if (!String.IsNullOrEmpty(group.Description))
+                request.AddParameter("description", group.Description);
+            if (group.Acl != null)
+                request.AddParameter("acl", group.Acl.Stringify());
+            if (group.Users.Count > 0)
+                request.AddParameter("user_ids", group.GetUserIds());
+
+            Group updated_group = new Group();
+
+            try
+            {
+                IRestResponse httpResponse = RestClient.Execute(request);
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    updated_group = JsonConvert.DeserializeObject<Group>(httpResponse.Content, TSCloud.serializer_settings());
+                    updated_group.StatusCode = httpResponse.StatusCode;
+
+                    return updated_group;
+                }
+                else
+                {
+                    updated_group.StatusCode = httpResponse.StatusCode;
+                    updated_group.Message = httpResponse.ErrorMessage;
+
+                    return updated_group;
+                }
+            }
+            catch (Exception ee)
+            {
+                return new Group(Convert.ToString(ee));
+            }
         }
 
         public Group Get(int GroupID)
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest(String.Format("{0}/groups/{1}", ApiPath, Convert.ToString(GroupID)), Method.GET);
+            request.AddParameter("api_token", ApiToken);
+
+            Group group = new Group();
+            try
+            {
+                IRestResponse httpResponse = RestClient.Execute(request);
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    group = JsonConvert.DeserializeObject<Group>(httpResponse.Content, TSCloud.serializer_settings());
+                    group.StatusCode = httpResponse.StatusCode;
+
+                    return group;
+                }
+                else
+                {
+                    group.StatusCode = httpResponse.StatusCode;
+                    group.Message = httpResponse.ErrorMessage;
+
+                    return group;
+                }
+            }
+            catch (Exception ee)
+            {
+                return new Group(Convert.ToString(ee));
+            }
         }
 
         public Group Copy(Group group)
